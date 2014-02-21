09ac68f5526e90554b5c2292b753a19acc625486
Masahiro Nagano <kazeburo@gmail.com>
2014-02-21 17:39:40 +0900

    Checking in changes prior to tagging of version 0.15.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 8eeb407..9ee36bd 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension WWW-Form-UrlEncoded-XS
    
     {{$NEXT}}
    
    +0.15 2014-02-21T08:39:34Z
    +
    +    - optimze around memory alloc
    +
     0.14 2014-02-21T04:11:13Z
    
         - add build_urlencoded_utf8

--- lib/WWW/Form/UrlEncoded/XS.pm
+++ lib/WWW/Form/UrlEncoded/XS.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use base qw/Exporter/;
 
-our $VERSION = "0.14";
+our $VERSION = "0.15";
 our @EXPORT_OK = qw/parse_urlencoded build_urlencoded build_urlencoded_utf8/;
 
 use XSLoader;
